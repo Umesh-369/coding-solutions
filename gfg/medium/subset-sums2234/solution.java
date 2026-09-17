2516b4a3c9888ebfaf2e99aca9f3677cfdb1cc3f
Umesh Sai Hanuma Prasad Syamala <s.umeshsaihanumaprasad@gmail.com>
2026-09-17 19:01:44 +0530

    ✅ Subset Sums | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ gfg/medium/subset-sums2234/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+   
+   public void func(int ind,int sum,int[] arr,int n,ArrayList<Integer> subset){
+       
+     if(ind==n){
+         subset.add(sum);
+         return ;
+     }    
+     
+     func(ind+1,sum+arr[ind],arr,n,subset);
+     
+     func(ind+1,sum,arr,n,subset);
+   }
+   
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        ArrayList<Integer> subset=new ArrayList<>();
+        func(0,0,arr,arr.length,subset);
+        Collections.sort(subset);
+        
+        return subset;
+    }
+}
